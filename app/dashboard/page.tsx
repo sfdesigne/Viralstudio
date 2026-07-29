dfa877e62514d3eb231ba75656bc8c513cd67547
Salim <49446742+sfdesigne@users.noreply.github.com>
2026-07-29 23:48:12 +0100

    Create Dashboard component with schedule information

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,4 @@
+'use client'
+export default function Dashboard(){
+  return <div style={{padding:20}}><h1>Dashboard - LIVE 5x per day</h1><p>10am, 1pm, 4pm, 7pm, 10pm</p><a href="/">Back Home</a></div>
+}
