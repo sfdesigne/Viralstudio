f945e40b132f44de5a82f685cb6033a5aa02593d
Salim <49446742+sfdesigne@users.noreply.github.com>
2026-07-29 23:42:56 +0100

    Delete app/app/dashboard directory

--- app/app/dashboard/page.tsx
+++
@@ -1,4 +0,0 @@
-'use client'
-export default function Dashboard(){
-  return <div style={{padding:20}}><h1>Dashboard - LIVE 5x per day</h1><p>10am, 1pm, 4pm, 7pm, 10pm</p><a href="/">Back Home</a></div>
-}
